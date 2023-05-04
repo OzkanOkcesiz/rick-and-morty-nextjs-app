a6030ec1f1e5af3f9583f2440ece490a23bda7fb
Ozkan <ozkan.okcesiz@gmail.com>
2023-05-04 05:04:44 +0300

    add Page

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,9 +1,14 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
     return (
         <div className='mb-8'>
-            <h1 className='text-cyan-100 text-xl text-center'>Rick and Morty Character App</h1>
+            <h1 className='text-cyan-100 text-xl text-center'>
+                <Link href={"/"}>
+                    Rick and Morty Character App
+                </Link>
+            </h1>
         </div>
     )
 }
---
+++ components/Page/index.tsx
@@ -0,0 +1,40 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import React from 'react'
+
+const Page = () => {
+    const router = useRouter()
+    const page = Number(router.query.page) || 1
+    const filter = router.query.filterName
+
+
+    return (
+        <div className='text-center'>
+            {page > 1 && (
+                <Link href={filter ? {
+                    pathname: `/${page - 1}`,
+                    query: {
+                        filterName: filter
+                    }
+                } : {
+                    pathname: `/${page - 1}`,
+                }} passHref>
+                    <button className='text-cyan-100 m-2'>Previous Page</button>
+                </Link>
+            )}
+
+            <Link href={filter ? {
+                pathname: `/${page + 1}`,
+                query: {
+                    filterName: filter
+                }
+            } : {
+                pathname: `/${page + 1}`,
+            }} passHref>
+                <button className='text-cyan-100 m-2'>Next Page</button>
+            </Link>
+        </div>
+    )
+}
+
+export default Page
